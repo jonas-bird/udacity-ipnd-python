72200c58a59d9d25096b3abd30ee29fe2a47f9e3
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-04 12:46:08 -0500

    Refactor order_breakfast.py
    Refactor both breakfast ordering examples to use a function print_pause
    instead of using separate print and time.sleep statements

--- style/example_bot.py
+++ style/example_bot.py
@@ -2,43 +2,38 @@
 
 import time
 
-print("Hello! I am Bob, the Breakfast Bot.")
-time.sleep(2)
-print("Today we have two breakfasts available.")
-time.sleep(2)
-print("The first is waffles with strawberries and whipped cream.")
-time.sleep(2)
-print("The second is sweet potato pancakes with butter and syrup.")
-time.sleep(2)
+
+def print_pause(msg):
+    print(msg)
+    time.sleep(2)
+
+
+print_pause("Hello! I am Bob, the Breakfast Bot.")
+print_pause("Today we have two breakfasts available.")
+print_pause("The first is waffles with strawberries and whipped cream.")
+print_pause("The second is sweet potato pancakes with butter and syrup.")
 
 while True:
     while True:
         response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
         if "waffles" in response:
-            print("Waffles it is!")
-            time.sleep(2)
+            print_pause("Waffles it is!")
             break
         elif "pancakes" in response:
-            print("Pancakes it is!")
-            time.sleep(2)
+            print_pause("Pancakes it is!")
             break
         else:
-            print("Sorry, I don't understand.")
-            time.sleep(2)
-    print("Your order will be ready shortly.")
-    time.sleep(2)
+            print_pause("Sorry, I don't understand.")
+    print_pause("Your order will be ready shortly.")
     while True: # We want to loop until they enter a valid response.
         order_again = input("Would you like to place another order? Please say 'yes' or 'no'.\n").lower()
         if 'no' in order_again:
-            print("OK, goodbye!")
-            time.sleep(2)
+            print_pause("OK, goodbye!")
             break # Note that this will only break out of the inner loop.
         elif 'yes' in order_again:
-            print("Very good, I'm happy to take another order.")
-            time.sleep(2)
+            print_pause("Very good, I'm happy to take another order.")
             break # Again, this only breaks out of the inner loop.
         else:
-            print("Sorry, I don't understand.")
-            time.sleep(2)
+            print_pause("Sorry, I don't understand.")
     if 'no' in order_again:
         break # We need this last break statement to exit the outer loop.
--- style/order-breakfast.py
+++ style/order-breakfast.py
@@ -3,48 +3,45 @@
 import time
 
 
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
+
 
 def order():
     while True:
         breakfast = input(
           "Please place your order. Would you like waffles or pancakes?\n").lower()
         if 'waffles' in breakfast:
-            print("Waffles it is!")
-            time.sleep(1)
+            print_pause("Waffles it is!")
             break
         elif 'pancakes' in breakfast:
-            print('Pancakes it is!')
-            time.sleep(1)
+            print_pause('Pancakes it is!')
             break
         else:
-            print("Sorry I did not understand that.")
+            print_pause("Sorry I did not understand that.")
 
-    print('Your order will be ready shortly.')
+    print_pause('Your order will be ready shortly.')
 
 
 def main():
-    print('Hello! I am Bob, the Breakfast Bot.')
-    time.sleep(2)
-    print('Today we have two breakfasts available.')
-    time.sleep(2)
-    print('The first is waffles with strawberries and whipped cream.')
-    time.sleep(2)
-    print('The second is sweet potato pancakes with butter and syrup.')
+    print_pause('Hello! I am Bob, the Breakfast Bot.')
+    print_pause('Today we have two breakfasts available.')
+    print_pause('The first is waffles with strawberries and whipped cream.')
+    print_pause('The second is sweet potato pancakes with butter and syrup.')
 
     order()
     while True:
         new_order = input(
           "Would you like to place another order? Please say 'yes' or 'no'.\n").lower()
         if 'no' in new_order:
-            print("OK. goodbye!")
-            time.sleep(2)
+            print_pause("OK. goodbye!")
             break
         elif 'yes' in new_order:
-            print("Very good, I'm happy to take another order.")
-            time.sleep(2)
+            print_pause("Very good, I'm happy to take another order.")
             order()
         else:
-            print("Sorry, I don't understand.")
+            print_pause("Sorry, I don't understand.")
             continue
 
 if __name__ == '__main__':
