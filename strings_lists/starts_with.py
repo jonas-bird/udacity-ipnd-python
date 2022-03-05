5e48bd814eda9508686cf3171dbb804199c76f1a
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-04 21:31:21 -0500

    Add starts_with.py
    Add starts_with.py, about a third of the way through lesson 5: strings and
    lists section

---
+++ strings_lists/starts_with.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Add your function definition here
+def starts_with(word1, word2):
+    return word1[0] == word2[0]
+# A call like this should return True:
+print(starts_with("banana", "bread"))
+
+# And one like this should return False:
+print(starts_with("zebonkey", "kiwi"))
