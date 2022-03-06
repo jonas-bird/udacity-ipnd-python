845621388c8f664ec0a1e7db7af4c75730b9a2c3
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-06 09:40:06 -0500

    Add substring.oy
    Represents work up to finding substrings 2/4

---
+++ strings_lists/substring.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+def is_substring(a_substring, a_string):
+
+    length = len(a_substring)
+    for i in range(len(a_string) - length):
+        if a_string[i:i+length] == a_substring:
+            return True
+    return False
+
+
+def count_substring(string, target):
+    index = 0
+    total = 0
+    while index < len(string):
+        if string[index:index+len(target)] == target:
+            total += 1
+        index += 1
+    return total
