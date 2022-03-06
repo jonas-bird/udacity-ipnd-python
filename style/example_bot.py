b7531ff141909e21a1183ce2c68660fe2ed84202
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-06 12:28:34 -0500

    Modify example_bot.py
    Refactor to use a function validate_order to validate user input

--- style/example_bot.py
+++ style/example_bot.py
@@ -4,8 +4,23 @@ import time
 
 
 def print_pause(msg):
+    """Takes a parameter, prints it, and waits 2 seconds"""
     print(msg)
     time.sleep(2)
+
+
+def validate_input(prompt, possible_choices):
+    """
+    Prompts the user for a response and checks if the input is in a list
+    provided by the second parameter
+    """
+    while True:
+        answer = input(prompt)
+        for option in possible_choices:
+            if option in answer:
+                return option
+
+        print_pause("Sorry, I don't understand.")
 
 
 print_pause("Hello! I am Bob, the Breakfast Bot.")
@@ -14,26 +29,15 @@ print_pause("The first is waffles with strawberries and whipped cream.")
 print_pause("The second is sweet potato pancakes with butter and syrup.")
 
 while True:
-    while True:
-        response = input("Please place your order. Would you like waffles or pancakes?\n").lower()
-        if "waffles" in response:
-            print_pause("Waffles it is!")
-            break
-        elif "pancakes" in response:
-            print_pause("Pancakes it is!")
-            break
-        else:
-            print_pause("Sorry, I don't understand.")
+    valid_choices = ['waffles', 'pancakes']
+    prompt = "Please place your order. Would you like waffles or pancakes?\n"
+    choice = validate_input(prompt, valid_choices)
+    print_pause(f"{choice} it is!")
     print_pause("Your order will be ready shortly.")
-    while True: # We want to loop until they enter a valid response.
-        order_again = input("Would you like to place another order? Please say 'yes' or 'no'.\n").lower()
-        if 'no' in order_again:
-            print_pause("OK, goodbye!")
-            break # Note that this will only break out of the inner loop.
-        elif 'yes' in order_again:
-            print_pause("Very good, I'm happy to take another order.")
-            break # Again, this only breaks out of the inner loop.
-        else:
-            print_pause("Sorry, I don't understand.")
-    if 'no' in order_again:
-        break # We need this last break statement to exit the outer loop.
+    again_choices = ['yes', 'no']
+    prompt_again = "Would you like to place another order? Please say 'yes' or 'no'.\n"
+    order_again = validate_input(prompt_again, again_choices)
+    if order_again == 'no':
+        print_pause("OK, goodbye!")
+        break
+    print_pause("Very good, I'm happy to take another order.")
