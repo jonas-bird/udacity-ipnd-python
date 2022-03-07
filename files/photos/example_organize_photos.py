7c0d8afc84835e1a583b7a3106bb911bf56e30a3
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-07 14:37:04 -0500

    Modify example_organize_photos,py
    Modify example_organize_photos.py to take command line arguments of the
    list of folders (located in the same directory as the script) to
    organize
    
    Refactor to use __name__=='__main__'

--- files/photos/example_organize_photos.py
+++ files/photos/example_organize_photos.py
@@ -2,9 +2,15 @@
 
 """
 Code based on examples given in Udacity's Intro to Programming Nano-degree
+Modified so that it takes command line arguments of subdirectories of the
+directory you run it in. It iterates through the files in the specified
+subdirectories and extracts the first word with underscores '_' on either
+side and creates a sub directory of that name and moves the file into it.
+
 """
 
 import os
+import sys
 
 
 def extract_place(f_name):
@@ -19,18 +25,27 @@ def make_place_directories(d_names):
         os.mkdir(d_name)
 
 
-os.chdir("Photos")
-originals = os.listdir()
-places = []
+def organize_photos(directory):
+    os.chdir(directory)
+    originals = os.listdir()
+    places = []
 
-for filename in originals:
-    place = extract_place(filename)
-    if place not in places:
-        places.append(place)
+    for filename in originals:
+        place = extract_place(filename)
+        if place not in places:
+            places.append(place)
 
-make_place_directories(places)
-# print(os.listdir())
+    make_place_directories(places)
 
-for filename in originals:
-    place = extract_place(filename)
-    os.rename(filename, os.path.join(place, filename))
+    for filename in originals:
+        place = extract_place(filename)
+        os.rename(filename, os.path.join(place, filename))
+
+
+def main(argv):
+    for directory in argv:
+        organize_photos(directory)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
