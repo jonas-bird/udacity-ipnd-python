5e6a3c5b38778bde47e79c4c3446b5f191b40258
Jonas Bird <91971493+jonas-bird@users.noreply.github.com>
2022-03-09 12:24:43 -0500

    Modify slow_modified_rudewords.py
    Up to date with example code through One word at a time
    Refactor so that code is in functions

--- filter/slow_modified_rudewords.py
+++ filter/slow_modified_rudewords.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
+# Rude words detector, version 0.2
+
 
 rude_words = ["crap", "darn", "heck", "jerk", "idiot", "butt", "devil"]
 
-with open("my_story.txt") as myfile:
-    contents = myfile.read()
-    rude_count = 0
-    for rude in rude_words:
-        if rude in contents:
-            rude_count += 1
-            print(f"Found rude word: {rude}")
-if rude_count == 0:
-    print("Congratulations, your file has no rude words.")
-    print("At least, no rude words I know.")
+
+def check_line(line):
+    count = 0
+    words = line.split(" ")
+    for word in words:
+        if word in rude_words:
+            count += 1
+            print(f"Found rude word: {word}")
+    return count
+
+
+def check_file(filename):
+    with open(filename) as myfile:
+        rude_count = 0
+        for line in myfile:
+            rude_count += check_line(line)
+    if rude_count == 0:
+        print("Congratulations, your file has no rude words.")
+        print("At least, no rude words I know.")
+
+
+if __name__ == "__main__":
+    check_file("my_story.txt")
